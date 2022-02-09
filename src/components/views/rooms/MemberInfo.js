5aca0b35e6ff474c1f54a5656765c8a3735e167c
Julien DAUPHANT <1238254+jdauphant@users.noreply.github.com>
2022-02-09 19:22:38 +0100

    Fix missing avatar

--- src/components/views/rooms/MemberInfo.js
+++ src/components/views/rooms/MemberInfo.js
@@ -903,7 +903,7 @@ module.exports = withMatrixClient(React.createClass({
         const avatarUrl = this.props.member.getMxcAvatarUrl();
         let avatarElement;
         if (avatarUrl) {
-            const httpUrl = this.props.matrixClient.mxcUrlToHttp(avatarUrl, 800, 800);
+            const httpUrl = this.props.matrixClient.mxcUrlToHttp(avatarUrl, 800, 800, 'crop', false);
             const scImageUrl = ContentScanner.getUnencryptedContentUrl({url : Tchap.imgUrlToUri(httpUrl)}, true);
             avatarElement = <div className="mx_MemberInfo_avatar">
                 <img src={scImageUrl}  alt="Image"/>
